4794df5dc2c535a34fe8682274aace0f8dcf026e
PAULO COSTA <paulofernando2830@gmail.com>
2024-07-31 18:28:14 -0300

    generate a band name based on city and pet

---
+++ begginer/script1_bng/band_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator!")
+
+city_name = input("Which city you grow up in?\n")
+pet_name = input("What's the name of your pet?\n")
+
+print("Your band name could be: " + city_name + " " + pet_name)
