8d6b9e6a03d02b6cae73812b9247b3a629a03250
PAULO COSTA <paulofernando2830@gmail.com>
2024-08-01 21:54:28 -0300

    study material

---
+++ begginer/script2_tipcalc/math_operator.py
@@ -0,0 +1,17 @@
+print(3 + 5)
+print(3 - 5)
+print(3 * 5)
+print(3 / 5)
+print(3 ** 5)
+print(3 % 5)
+
+'''
+PEMDAS
+1.() parenthesis
+2.** exponents
+3.* multiplication
+4./ division
+5.+ addition
+6.- subtraction
+
+'''
---
+++ begginer/script2_tipcalc/primitive_data_types.py
@@ -0,0 +1,25 @@
+# STRING
+str = "Hello!"
+print(str)
+
+# INTEGER
+int = 43456
+print(int)
+
+# FLOAT
+float = 3.1415
+print(float)
+
+# BOOLEAN
+True
+False
+
+# Type function >> type() to check type
+# How to change a data type
+a = 123
+type(a) #output: int
+a = str(123) # output: str
+print(type(a))
+# float()
+b = float(123)
+print(type(b))
